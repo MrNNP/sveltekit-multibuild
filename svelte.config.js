12a3f13b7f04a2aaa33454119c7ddd9c38d74f78
MrNNP <reachnp64@gmail.com>
2023-05-22 21:32:56 -0400

    add electron adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@ptkdev/sveltekit-electron-adapter';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build/electron',
+			assets: 'build/electron',
+			fallback: null,
+			precompress: true,
+			strict: true,
+			policy: "default-src 'self'; script-src 'self'",
+			viewport: `width=device-width`
+		})
 	}
 };
 
