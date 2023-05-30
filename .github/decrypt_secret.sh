74f3f2b2b219409846410342a9f393e9722d794f
MrNNP <reachnp64@gmail.com>
2023-05-30 13:57:25 -0400

    rename from extention to extension

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -4,4 +4,4 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_DECRYPTION_KEY" \
---output browser-extention.pem .//.github/secrets/browser-extention.pem.gpg
+--output browser-extension.pem .//.github/secrets/browser-extension.pem.gpg
--- browser-extension/sw2.js
+++ browser-extension/sw2.js
@@ -1,7 +1,6 @@
 chrome.action.onClicked.addListener((tab) => {
 	chrome.tabs.create({
 		url: chrome.runtime.getURL('build/index.html')
-		//In here you can also add constrain for the window
-		//This is for manifest v3
+
 	});
 });
