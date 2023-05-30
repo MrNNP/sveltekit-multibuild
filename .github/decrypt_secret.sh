093155da7e36c6e0ba6a27d83644b5bcc08de1f3
MrNNP <reachnp64@gmail.com>
2023-05-30 15:09:52 -0400

    ios build

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -5,3 +5,7 @@
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_DECRYPTION_KEY" \
 --output browser-extension.pem .//.github/secrets/browser-extension.pem.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_DECRYPTION_KEY" \
+--output ios_distribution.p12 .//.github/secrets/ios_distribution.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_DECRYPTION_KEY" \
+--output SvelteKit_Multibuild_Provision.mobileprovisi .//.github/secrets/SvelteKit_Multibuild_Provision.mobileprovisi.gpg
